1938e543e855bcb9016b489636afe254057bf98f
Siddhant Lad <32464449+SiddhantL@users.noreply.github.com>
2024-11-24 04:28:23 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -24,7 +24,7 @@ import google.auth.transport.requests
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 import requests
-from calendarGoogle import calendarGoogle
+from src.calendarGoogle import calendarGoogle
 
 
 # Load environment variables from .env file
