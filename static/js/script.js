53e0abed4a59dfbab88315dba24112f16dffebf5
Shelby Myrman <myrmanshelby@gmail.com>
2024-11-28 14:24:11 -0800

    fix #46 - added light/dark mode toggle

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,9 @@ document.querySelector(".logout-btn").addEventListener("click", function () {
     // Redirect to the Flask logout route
     window.location.href = "/logout";
 });
+
+
+let themeToggler = document.querySelector('#theme-toggle');
+themeToggler.addEventListener('click', function(){ 
+    document.body.classList.toggle('darkMode');
+});
