9e1dbb893d6c2b2da07411a6cdad1c5a6b85469e
Shelby Myrman <myrmanshelby@gmail.com>
2024-11-27 23:50:07 -0800

    fix #41 - fixed styling and created templates folder for index and sign in

---
+++ static/js/signIn.js
@@ -0,0 +1,4 @@
+document.querySelector(".signin-btn").addEventListener("click", function () {
+    // Placeholder for Google OAuth Integration
+    alert("Google OAuth Integration Goes Here.");
+});
