46f3732fff1e1a1f55d7eea1ad2f10bd3145f95b
Shelby Myrman <myrmanshelby@gmail.com>
2024-11-27 23:17:21 -0800

    Added simple sign in html and css

--- src/app.py
+++ src/app.py
@@ -21,7 +21,7 @@ import google.auth.transport.requests
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 import requests
-from src.calendarGoogle import calendarGoogle
+from calendarGoogle import calendarGoogle
 
 
 # Load environment variables from .env file
