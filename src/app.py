b0b131baea22ec00bb38cac5c274a7cd2191f782
Maitree Varia <maitree@Maitrees-MacBook-Air.local>
2024-12-06 01:15:41 -0800

    Fix iframe problem

--- src/app.py
+++ src/app.py
@@ -56,7 +56,7 @@ REDIRECT_URI = os.environ.get("REDIRECT_URI", "http://127.0.0.1:5000/callback")
 
 # Path to the client secrets JSON file downloaded from Google Cloud Console
 CLIENT_SECRETS_FILE = os.path.join(
-    pathlib.Path(__file__).parent, "/etc/secrets/client_secret.json"
+    pathlib.Path(__file__).parent, "client_secret.json"
     )
 
 # OAuth 2.0 scopes (including Calendar API scopes)
@@ -264,6 +264,16 @@ def dashboard():
         Response: Renders the index.html template.
     """
     return render_template("index.html")
+
+@app.route("/internshipTracker")
+def internship_tracker():
+    """
+    Internship tracker page.
+
+    Returns:
+        Response: Renders the InternshipTracker.html template.
+    """
+    return render_template("InternshipTracker.html")
 
 
 @app.route('/calendar.html')
