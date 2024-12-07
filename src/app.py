d393ba8d47233baa14e43174419e91f4b4c76229
Siddhant Lad <32464449+SiddhantL@users.noreply.github.com>
2024-12-06 21:01:06 -0800

    Update app.py
    Fix imports

--- src/app.py
+++ src/app.py
@@ -23,6 +23,8 @@ from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 import requests
 from src.calendarGoogle import calendarGoogle
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.mutable import MutableDict
 from datetime import datetime
 
 # Load environment variables from .env file
