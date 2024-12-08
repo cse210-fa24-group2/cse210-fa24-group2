a345b7b18ed3d784ed9db21d51eec28d1bd4a79d
Siddhant Lad <32464449+SiddhantL@users.noreply.github.com>
2024-12-08 02:20:46 -0800

    Update app.py
    Client secret update path

--- src/app.py
+++ src/app.py
@@ -58,7 +58,7 @@ REDIRECT_URI = os.environ.get("REDIRECT_URI", "http://127.0.0.1:5000/callback")
 
 # Path to the client secrets JSON file downloaded from Google Cloud Console
 CLIENT_SECRETS_FILE = os.path.join(
-    pathlib.Path(__file__).parent, "client_secret.json"
+    pathlib.Path(__file__).parent, "/etc/secrets/client_secret.json"
     )
 
 # OAuth 2.0 scopes (including Calendar API scopes)
