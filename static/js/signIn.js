f5426108537f8f019e34f8372d73e94e979aeb39
Shelby Myrman <myrmanshelby@gmail.com>
2024-12-08 15:19:45 -0800

    Added dark mode to sign in page and added logo

--- static/js/signIn.js
+++ static/js/signIn.js
@@ -2,3 +2,8 @@ document.querySelector(".signin-btn").addEventListener("click", function () {
     // Redirect to the Flask login route
     window.location.href = "/login";
 });
+
+document.querySelector('#theme-toggle').addEventListener('click', function(){ 
+    // Toggles dark mode and light mode when button is clicked
+    document.body.classList.toggle('darkMode');
+});
