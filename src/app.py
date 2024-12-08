99bbc4189237fecbd8182df5e58c70294337a47a
Shelby Myrman <myrmanshelby@gmail.com>
2024-12-08 14:34:33 -0800

    fix #58 - mobile optimimized site outline
    Individual components are not yet optimized, but site skeleton is

--- src/app.py
+++ src/app.py
@@ -22,7 +22,7 @@ import google.auth.transport.requests
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 import requests
-from src.calendarGoogle import calendarGoogle
+from calendarGoogle import calendarGoogle
 
 
 # Load environment variables from .env file
@@ -56,7 +56,7 @@ REDIRECT_URI = os.environ.get("REDIRECT_URI", "http://127.0.0.1:5000/callback")
 
 # Path to the client secrets JSON file downloaded from Google Cloud Console
 CLIENT_SECRETS_FILE = os.path.join(
-    pathlib.Path(__file__).parent, "/etc/secrets/client_secret.json"
+    pathlib.Path(__file__).parent, "client_secret.json"
     )
 
 # OAuth 2.0 scopes (including Calendar API scopes)
@@ -200,6 +200,7 @@ def home():
     Returns:
         str: HTML content with options.
     """
+    return render_template("index.html")
     if "id_google" not in session:
         return render_template("signIn.html")
     else:
