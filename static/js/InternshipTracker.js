070b2933972e31f3f42447b7a17eb1cdaa11646a
Shelby Myrman <myrmanshelby@gmail.com>
2024-12-09 19:35:24 -0800

    Career tracker mobile responsive testing

--- static/js/InternshipTracker.js
+++ static/js/InternshipTracker.js
@@ -224,6 +224,16 @@ document.addEventListener("DOMContentLoaded", () => {
                         `<div class="edit" onclick='editInternship(${JSON.stringify(item)})'>✎</div>`,
                         `<div class="delete" onclick='deleteInternship(${JSON.stringify(item)})'>❌</div>`
                     ])
+                },
+                responsive: {
+                    details: {
+                        type: 'column', // Add a control column for expanded details
+                        target: 0 // Set the control column to the first column
+                    },
+                    breakpoints: [
+                        { width: 950, columns: [1, 2] }, // Show only the first two columns on screens smaller than 768px
+                        { width: 0, columns: [] } // Hide all columns for very small screens
+                    ]
                 }
             });
 
