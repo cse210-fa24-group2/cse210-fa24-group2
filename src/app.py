703721dcc12407b9e9596da89e9165bd23fd0d50
Maitree Varia <maitree@Maitrees-Air.lan1>
2024-12-11 15:19:32 -0800

    added src

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 """
 app.py
 
-This module implements a Flask web application that integrates Google 
-OAuth 2.0 authentication, PostgreSQL database management using SQLAlchemy, 
-and provides API routes for managing user data, internships, and to-do lists.
+This module implements a Flask web application that integrates Google OAuth 2.0
+authentication, PostgreSQL database management using SQLAlchemy, and provides
+API routes for managing user data, internships, and to-do lists.
 
 Features:
 - Google OAuth 2.0 login and logout functionality.
@@ -29,7 +29,7 @@ import google.auth.transport.requests
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 import requests
-from calendarGoogle import calendarGoogle
+from src.calendarGoogle import calendarGoogle
 from datetime import datetime
 
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -54,7 +54,7 @@ GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
 REDIRECT_URI = os.environ.get("REDIRECT_URI")
 
 CLIENT_SECRETS_FILE = os.path.join(
-    pathlib.Path(__file__).parent, "client_secret.json"
+    pathlib.Path(__file__).parent, "/etc/secrets/client_secret.json"
     )
 
 SCOPES = [
